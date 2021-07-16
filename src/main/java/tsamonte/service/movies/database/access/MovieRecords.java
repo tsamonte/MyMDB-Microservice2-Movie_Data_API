fa32d296af079e138a0c41b64d7843b124c90c3d
Tristan Samonte <tsamonte23@gmail.com>
2021-07-16 16:10:50 -0700

    Added functions for /api/movies/browse/{phrase} database access

--- src/main/java/tsamonte/service/movies/database/access/MovieRecords.java
+++ src/main/java/tsamonte/service/movies/database/access/MovieRecords.java
@@ -5,6 +5,7 @@ import tsamonte.service.movies.database.model.movie.MovieModel;
 import tsamonte.service.movies.database.model.movie.SearchBrowseModel;
 import tsamonte.service.movies.database.model.movie.ThumbnailModel;
 import tsamonte.service.movies.logger.ServiceLogger;
+import tsamonte.service.movies.models.queryparameter.MovieBrowseQueryModel;
 import tsamonte.service.movies.models.queryparameter.MovieSearchQueryModel;
 
 import java.sql.PreparedStatement;
@@ -55,7 +56,7 @@ public class MovieRecords {
      *      - /api/movies/search
      *
      * @param queryModel An object containing info about the passed in query parameters
-     * @return ResultSet containing data retreived from the database. Can be null.
+     * @return ResultSet containing data retrieved from the database. Can be null.
      */
     private static ResultSet getResult(MovieSearchQueryModel queryModel) {
         try {
@@ -93,6 +94,17 @@ public class MovieRecords {
         }
     }
 
+    /**
+     * Using the previous two functions (buildQuery and getResult), this function builds the SQL query based on the passed
+     * in query model. The query is called upon the database, and the results are mapped to objects that will be returned
+     * here.
+     *
+     * Related endpoints :
+     *      - /api/movies/search
+     *
+     * @param queryModel An object containing info about the passed in query parameters
+     * @return A list of objects that model an encapsulating field in the endpoint's response JSON
+     */
     public static SearchBrowseModel[] retrieve(MovieSearchQueryModel queryModel) {
         try {
             ArrayList<SearchBrowseModel> results = new ArrayList<SearchBrowseModel>();
@@ -123,6 +135,120 @@ public class MovieRecords {
         }
     }
 
+    // ================================================/API/MOVIES/BROWSE/{PHRASE}================================================
+    /**
+     * Builds a MySQL query that will be passed into a prepared statement. WHERE conditions of the query will be added based
+     * on what is passed in through the queryModel parameter
+     *
+     * Related endpoints :
+     *      - /api/movies/browse/{phrase}
+     *
+     * @param queryModel An object containing info about the passed in query parameters
+     * @return A string representing a SQL statement before being passed into a PreparedStatement.
+     */
+    private static String buildQuery(MovieBrowseQueryModel queryModel) {
+        String SELECT = "SELECT DISTINCT m.movie_id, m.title, m.year, p.name, m.rating, m.backdrop_path, m.poster_path, m.hidden";
+        String FROM = " FROM movie AS m INNER JOIN person AS p ON m.director_id = p.person_id";
+        String WHERE = " WHERE 1=1";
+        String ORDERBY = " ORDER BY " + queryModel.getOrderBy() + " " + queryModel.getDirection();
+        String LIMIT = " LIMIT " + queryModel.getLimit() + " OFFSET " + queryModel.getOffset();
+
+        // Add keywords to database query
+        if(queryModel.getKeywords() != null || !queryModel.getKeywords().isEmpty()) {
+            for(int i = 0; i < queryModel.getKeywords().size(); i++) {
+                FROM += String.format(" INNER JOIN keyword_in_movie AS kim%d ON m.movie_id = kim%d.movie_id", i, i);
+                FROM += String.format(" INNER JOIN keyword AS k%d ON k%d.keyword_id = kim%d.keyword_id", i, i, i);
+
+                WHERE += String.format(" AND k%d.name = ?", i);
+            }
+        }
+
+        // If query param says not to show hidden, then regardless of plevel, only show rows where m.hidden = false
+        if(queryModel.showHidden() == null || !queryModel.showHidden()) WHERE += " AND hidden = FALSE";
+
+        // Secondary sort
+        if(queryModel.getOrderBy().equals("title") || queryModel.getOrderBy().equals("year")) ORDERBY += ", rating DESC";
+        else if(queryModel.getOrderBy().equals("rating")) ORDERBY += ", title ASC";
+
+        return SELECT + FROM + WHERE + ORDERBY + LIMIT;
+    }
+
+    /**
+     * Prepares the query to be called and returns the ResultSet retrieved from the database. Returned ResultSet can be null.
+     *
+     * Related endpoints :
+     *      - /api/movies/browse/{phrase}
+     *
+     * @param queryModel An object containing info about the passed in query parameters
+     * @return ResultSet containing data retrieved from the database. Can be null.
+     */
+    private static ResultSet getResult(MovieBrowseQueryModel queryModel) {
+        try {
+            String query = buildQuery(queryModel);
+            PreparedStatement ps = MoviesService.getCon().prepareStatement(query);
+
+            ArrayList<String> keywords = queryModel.getKeywords();
+            for(int i = 0; i < keywords.size(); i++) {
+                ps.setString(i+1, keywords.get(i));
+            }
+
+            ServiceLogger.LOGGER.info("Trying query: " + ps.toString());
+            ResultSet rs = ps.executeQuery();
+            ServiceLogger.LOGGER.info("Query succeeded.");
+
+            return rs;
+        }
+        catch (SQLException e) {
+            ServiceLogger.LOGGER.warning("Query failed: Unable to retrieve movie records.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Using the previous two functions (buildQuery and getResult), this function builds the SQL query based on the passed
+     * in query model. The query is called upon the database, and the results are mapped to objects that will be returned
+     * here.
+     *
+     * Related endpoints :
+     *      - /api/movies/browse/{phrase}
+     *
+     * @param queryModel An object containing info about the passed in query parameters
+     * @return A list of objects that model an encapsulating field in the endpoint's response JSON
+     */
+    public static SearchBrowseModel[] retrieve(MovieBrowseQueryModel queryModel) {
+        try {
+            ArrayList<SearchBrowseModel> results = new ArrayList<SearchBrowseModel>();
+            ResultSet rs = getResult(queryModel);
+
+            if(rs != null) {
+                while (rs.next()) {
+                    results.add(new SearchBrowseModel(rs.getString("movie_id"),
+                            rs.getString("title"),
+                            rs.getInt("year"),
+                            rs.getString("name"),
+                            rs.getFloat("rating"),
+                            rs.getString("backdrop_path"),
+                            rs.getString("poster_path"),
+                            queryModel.showHidden() == null || !queryModel.showHidden() ? null : rs.getBoolean("hidden")
+                    ));
+                }
+            }
+
+            SearchBrowseModel[] resultsToArray = new SearchBrowseModel[results.size()];
+            resultsToArray = results.toArray(resultsToArray);
+            return resultsToArray;
+        }
+        catch (SQLException e) {
+            ServiceLogger.LOGGER.warning("Query failed: Unable to retrieve movie records.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // ================================================/API/MOVIES/GET/{MOVIE_ID}================================================
+
+    // ================================================/API/MOVIES/THUMBNAIL================================================
     public static MovieModel retrieve(String movie_id) {
         try {
             MovieModel result = null;
